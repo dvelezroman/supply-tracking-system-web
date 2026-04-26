c2e59daed08d41ef7566782f384a1bf908dce361
Dario Velez Roman <dario.velez@intive.com>
2026-04-25 21:04:20 -0500

    Update production environment configuration to use the new API base URL for the supply tracking system. This change ensures that the application connects to the correct backend service in the production environment.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBase: '/api/v0',
+  apiBase: 'https://supply-tracking-system-api.porto-rockcity-app.com/api/v0',
   labelLogoUrl: 'https://bitflow-public.s3.us-east-1.amazonaws.com/LogoCamaronera.png',
   labelBrandName: 'MAREA ALTA',
   bitflowLogoUrl: 'https://bitflow-public.s3.us-east-1.amazonaws.com/Bitflow-logo.png',
